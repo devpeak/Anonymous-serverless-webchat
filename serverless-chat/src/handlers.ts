efa8809c1ab73f557f43443ec5cb0681aaa9b104
devpeak <malaythakur13@gmail.com>
2023-02-28 11:10:54 +0530

    handle different route keys and handleConnect()

--- serverless-chat/src/handlers.ts
+++ serverless-chat/src/handlers.ts
@@ -1,17 +1,48 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyEventQueryStringParameters, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk"
 
+type Action = "$connect" | "$disconnect" | "getMessages" | "sendMessages" | "getClients";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
 export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  event.requestContext.routeKey
+  // wss://asds/aws.com?nickname=abc
+  const connectionId = event.requestContext.connectionId as string;
+  const routeKey = event.requestContext.routeKey as Action;
   
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
-  };
+  switch (routeKey) {
+    case "$connect":
+      return handleConnect(connectionId, event.queryStringParameters);
+   
+    default:
+      return {
+        statusCode: 500,
+        body: "",
+      };
+  }
 };
+
+const handleConnect = async(
+  connectionId:string,
+  queryParams: APIGatewayProxyEventQueryStringParameters | null,
+  ): Promise<APIGatewayProxyResult> => {
+    if (!queryParams || !queryParams["nickname"]){
+      return {
+        statusCode: 403,
+        body: "",
+      };
+    }
+
+    await docClient.put({
+      TableName: 'Clients',
+      Item: {
+        connectionId, 
+        nickname: queryParams["nickname"],
+      },
+     })
+     .promise();
+    
+    return {
+      statusCode: 200,
+      body: "",
+    }; 
+  };
